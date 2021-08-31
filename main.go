d9f7c2b76e2ea56d49f1bb720409f938e912b92a
Alfonso <vc.alfonso@hotmail.com>
2021-08-31 10:40:53 -0500

    feat: added main file

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which write a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Adda showSnippet handler function.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a createSnippet handler function.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the requedt is using POST or not. Note that
+	// http.MethodPost is a constant equal to the string "POST".
+	if r.Method != http.MethodPost {
+		// If it's  not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Methid"
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	// Use the http.NewServerMux() function to initialize a new servermux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the servermux we just created. If http.ListenAndServe() returns an error
+	// we use the log.Fatal function to log the error message and exit. Note
+	// that any error returned by http.ListenAndServe() is always non-nil
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
