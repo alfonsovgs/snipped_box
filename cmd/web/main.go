c791a4b03c6288968396fc9aae0fd08cf863a903
Alfonso <vc.alfonso@hotmail.com>
2021-09-03 11:52:03 -0500

    isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -47,27 +47,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Use the http.NewServerMux() function to initialize a new servermux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Create a file server wich servers files out of the "./ui/static" directory.
-	// Note that the path given to the htt.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching pathsm, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
---
+++ cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	// Use the http.NewServerMux() function to initialize a new servermux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	// Create a file server wich servers files out of the "./ui/static" directory.
+	// Note that the path given to the htt.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching pathsm, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
