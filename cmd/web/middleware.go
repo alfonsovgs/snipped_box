cf10ea3cedee1d0547066b3ff216de11ae53d498
Alfonso <vc.alfonso@hotmail.com>
2021-09-14 08:37:06 -0500

    request logging

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -10,3 +10,11 @@ func secureHeaders(next http.Handler) http.Handler {
 		next.ServeHTTP(rw, r)
 	})
 }
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(rw, r)
+	})
+}
